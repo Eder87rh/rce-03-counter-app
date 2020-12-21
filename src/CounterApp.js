6d1d4741d65f5bd0a2cd03d4d2aeedfc74675192
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2020-12-21 15:55:47 -0600

    handleSubstract & handleReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const CounterApp = ({ value }) => {
-  const handleApp = (e) => {
-    console.log("true", e);
+const CounterApp = ({ value = 10 }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleApp = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleReset = () => {
+    setCounter(value);
+  };
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
   };
 
   return (
     <div>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
       <button onClick={handleApp}>+1</button>
+      <button onClick={handleReset}>reset</button>
+      <button onClick={handleDecrement}>-1</button>
     </div>
   );
 };
